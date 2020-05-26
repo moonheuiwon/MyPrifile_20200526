6117c4c24158c713e31114a3920ed2c4682a4920
moonheuiwon <wonheuimoon@gmail.com>
2020-05-26 16:52:43 +0900

    라이브러리 사용 연습

---
+++ app/src/main/java/com/example/myprifile_20200526/BaseActivity.java
@@ -0,0 +1,13 @@
+package com.example.myprifile_20200526;
+
+import android.content.Context;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public abstract class BaseActivity extends AppCompatActivity {
+
+    public Context mContext = this;
+
+    public abstract void setupEvents();
+    public abstract void setValues();
+}
