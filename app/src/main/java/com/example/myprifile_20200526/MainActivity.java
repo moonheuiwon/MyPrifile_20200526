6a7cae17e900251ee56f0ee0222ca808b1f1d1de
moonheuiwon <wonheuimoon@gmail.com>
2020-05-27 09:56:53 +0900

    TedPermission 을 활용한 권한관리  - 전화걸수 있도록 짜둔 로직

--- app/src/main/java/com/example/myprifile_20200526/MainActivity.java
+++ app/src/main/java/com/example/myprifile_20200526/MainActivity.java
@@ -3,13 +3,20 @@ package com.example.myprifile_20200526;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.Manifest;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.example.myprifile_20200526.databinding.ActivityMainBinding;
 import com.github.chrisbanes.photoview.PhotoView;
+import com.gun0912.tedpermission.PermissionListener;
+import com.gun0912.tedpermission.TedPermission;
+
+import java.util.List;
 
 public class MainActivity extends BaseActivity {
 
@@ -25,6 +32,41 @@ public class MainActivity extends BaseActivity {
 
     @Override
     public void setupEvents() {
+
+        binding.callBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                PermissionListener pl = new PermissionListener() {
+                    @Override
+                    public void onPermissionGranted() {
+//                        허가가 된 상황 => 실제 전화걸기
+//                        실제 전화 걸기 => 권한 허가가 안남, 앱이 강제 종료됨 => TedPermission 권한 획득 후 사용
+                        String phoneNum = binding.phoneNumTxt.getText().toString();
+                        Uri myUri = Uri.parse(String.format("tel:%s", phoneNum));
+                        Intent myIntent = new Intent(Intent.ACTION_CALL, myUri);
+                        startActivity(myIntent);
+                    }
+
+                    @Override
+                    public void onPermissionDenied(List<String> deniedPermissions) {
+//                        최종적으로 거부되었을때 처리할 행동
+                        Toast.makeText(mContext, "권한이 거부되어 통화가 불가능합니다.", Toast.LENGTH_SHORT).show();
+                    }
+                };
+
+//                만약에 권한 설정이 안되어있으면 => 얼럿으로 허용할지 ?
+//                권한 이전 허용 => 곧바로 Granted 실행.
+                TedPermission
+                        .with(mContext)
+                        .setPermissionListener(pl)
+                        .setDeniedMessage("거부하면 통화가 불가능함. \n 설정에서 권한을 켜주세요.")
+                        .setPermissions(Manifest.permission.CALL_PHONE)
+                        .check();
+
+            }
+        });
+
         binding.profilImg.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
